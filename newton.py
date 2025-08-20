01b817b60340668035c8d05a38ff226d56c90a3b
Chris Paciorek <paciorek@berkeley.edu>
2025-08-20 13:41:08 -0700

    Lint newton.py. Add doc string.

--- newton.py
+++ newton.py
@@ -1,21 +1,25 @@
-def deriv(f, x, eps = 1e-16):
+def deriv(f, x, eps=1e-16):
     # if x < 5:
     #    raise RuntimeError("fake error")
-    return (f(x+eps) - f(x)) / eps
+    return (f(x + eps) - f(x)) / eps
 
 
-def deriv2(f, x, eps = 1e-16):
-    return (deriv(f, x+eps, eps) - deriv(f, x, eps)) / eps
+def deriv2(f, x, eps=1e-16):
+    return (deriv(f, x + eps, eps) - deriv(f, x, eps)) / eps
 
 
-def optimize(x0, f, tol =1e-4):
-    x_new = x0 - deriv(f, x0)/    deriv2(f, x0)
+def optimize(x0, f, tol=1e-4):
+    """Run Newton's method to minimize a function.
+
+    Parameters
+    ----------
+    x0: starting value
+    f: function to minimize
+    tol: blah blah blah
+    """
+    x_new = x0 - deriv(f, x0) / deriv2(f, x0)
     x = x0
     while abs(x_new - x) < tol:
         x = x_new
         x_new = x0 - deriv(f, x0) / deriv2(f, x0)
-    return {"x": x_new,
-            'value': f(x_new)}
-
-
-
+    return {"x": x_new, "value": f(x_new)}
